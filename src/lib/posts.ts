43af9036e4002389c7f29fa22b483449027368df
Michalis Zamparas <mikezaby@gmail.com>
2024-03-26 18:16:26 +0200

    Implement markdown posts metadata parsing

---
+++ src/lib/posts.ts
@@ -0,0 +1,34 @@
+import fs from "fs";
+import path from "path";
+
+const postsDirectory = path.join(process.cwd(), "src/app/posts");
+
+export interface PostMetadata {
+  id: string;
+  title: string;
+  date: string;
+  description: string;
+}
+
+const BLACKLIST_FILES = ["layout.tsx", "page.tsx"];
+
+export async function postsMetadata(): Promise<PostMetadata[]> {
+  const fileNames = fs.readdirSync(postsDirectory).filter((filename) => {
+    return !BLACKLIST_FILES.includes(filename);
+  });
+
+  const allPostsData = await Promise.all(
+    fileNames.map(async (fileName) => {
+      console.log(fileName);
+      const { metadata } = await import(`@/app/posts/${fileName}/page.mdx`);
+      return {
+        id: fileName,
+        ...metadata,
+      };
+    }),
+  );
+
+  return allPostsData.sort(({ date: a }, { date: b }) => {
+    return new Date(b).valueOf() - new Date(a).valueOf();
+  });
+}
