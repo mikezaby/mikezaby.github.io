1e8eda2dd32fcd0f9a5f4eb654febecb9cb85c3c
Michalis Zamparas <mikezaby@gmail.com>
2024-03-27 17:33:26 +0200

    Add footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Header } from "@/components";
+import { Footer } from "@/components/layout/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,9 +18,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={`${inter.className} flex min-h-screen flex-col`}>
         <Header />
-        <main className="container mt-8">{children}</main>
+        <main className="container mt-8 flex-grow">{children}</main>
+        <Footer />
       </body>
     </html>
   );
---
+++ src/components/layout/Footer.tsx
@@ -0,0 +1,9 @@
+export function Footer() {
+  return (
+    <footer className="container py-8 text-center">
+      <p className="text-gray-600 dark:text-gray-300">
+        © 2024 Michalis Zabaras. All rights reserved.
+      </p>
+    </footer>
+  );
+}
