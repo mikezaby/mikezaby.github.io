0aa351eaa25cc8bf15b7e893861852fa133338d7
Michalis Zamparas <mikezaby@gmail.com>
2024-03-27 13:35:08 +0200

    Add deployment packages and command

--- next.config.js
+++ next.config.js
@@ -2,9 +2,8 @@ const withMDX = require("@next/mdx")();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Configure `pageExtensions` to include MDX files
+  output: "export", // enables static exports
   pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
-  // Optionally, add any other Next.js config below
 };
 
 module.exports = withMDX(nextConfig);
