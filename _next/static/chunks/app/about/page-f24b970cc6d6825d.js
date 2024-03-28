b7e91b2797185e92df8ff9ccfc64fce6ca58749e
Michalis Zamparas <mikezaby@gmail.com>
2024-03-28 13:27:05 +0200

    Updates

---
+++ _next/static/chunks/app/about/page-f24b970cc6d6825d.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[301],{5198:function(e,n,s){Promise.resolve().then(s.t.bind(s,8139,23)),Promise.resolve().then(s.t.bind(s,9254,23))}},function(e){e.O(0,[139,254,146,672,744],function(){return e(e.s=5198)}),_N_E=e.O()}]);
