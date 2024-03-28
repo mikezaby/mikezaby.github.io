3ec03da2e81bac700ce562b281b06b5ac1f7cc6d
Michalis Zamparas <mikezaby@gmail.com>
2024-03-28 12:26:48 +0200

    Add image support for mdx

--- next.config.js
+++ next.config.js
@@ -4,6 +4,9 @@ const withMDX = require("@next/mdx")();
 const nextConfig = {
   output: "export", // enables static exports
   pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
+  images: {
+    unoptimized: true,
+  },
 };
 
 module.exports = withMDX(nextConfig);
--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -1,7 +1,12 @@
 import type { MDXComponents } from "mdx/types";
+import Image from "next/image";
+import { ComponentProps } from "react";
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
     ...components,
+    Image: (props: ComponentProps<typeof Image>) => (
+      <Image {...props} alt={props.alt} />
+    ),
   };
 }
