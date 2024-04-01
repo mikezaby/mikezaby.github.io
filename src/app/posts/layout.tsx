463c320169955a472aa23d9719bdb169d39ba26d
Michalis Zamparas <mikezaby@gmail.com>
2024-04-01 11:34:55 +0300

    Add disqus to the posts

--- src/app/posts/layout.tsx
+++ src/app/posts/layout.tsx
@@ -1,10 +1,21 @@
 "use client";
+
+import { DiscussionEmbed } from "disqus-react";
 import { MDXLayout } from "@/components";
-import { useCodeHighlighter } from "@/hooks";
+import { useCodeHighlighter, useDisqusConfig } from "@/hooks";
 import { ReactNode } from "react";
+
+const DISCUSS_SHORTNAME = "mikezaby";
 
 export default function Layout({ children }: { children: ReactNode }) {
   useCodeHighlighter();
+  const disqusConfig = useDisqusConfig();
 
-  return <MDXLayout>{children}</MDXLayout>;
+  return (
+    <div>
+      <MDXLayout>{children}</MDXLayout>
+
+      <DiscussionEmbed shortname={DISCUSS_SHORTNAME} config={disqusConfig} />
+    </div>
+  );
 }
