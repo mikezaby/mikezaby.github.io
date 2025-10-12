7fd993f4b02b7d409979ed6092202ad065a867ab
Michalis Zabaras <mikezaby@gmail.com>
2025-10-12 17:45:02 +0300

    Updates

---
+++ _next/static/chunks/pages/_app-a6b8c5e1f0414e49.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[888],{7289:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/_app",function(){return u(6884)}])}},function(n){var _=function(_){return n(n.s=_)};n.O(0,[774,179],function(){return _(7289),_(2202)}),_N_E=n.O()}]);
